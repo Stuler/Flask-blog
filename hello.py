26b23b3d8a39dab1e37969f0549a6ebf9a5229ee
Stuler <peter.jurek@gmail.com>
2025-02-23 12:49:51 +0100

    forms and flashes

--- hello.py
+++ hello.py
@@ -7,11 +7,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Gm8hFBfK'
 bootstrap = Bootstrap5(app)
 moment = Moment(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -25,19 +25,22 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
 
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
-
